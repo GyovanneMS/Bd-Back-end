886c82027d54f10f867669242392c053190e1eb8
GyovanneMS <Gyovannecontato@gmail.com>
2022-10-06 08:10:47 -0300

    Add all

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+npm install express --save
+npm install cors --save
+npm install body-parser --save
+-->   Para manipular o acesso ao BD podemos utilizar o Prisma, para instalar o prisma, devemos rodaros seguintes comandos: 
+npm install prisma --save
+npx prisma
+npx prisma init
+npm install @prisma/client
+*/
